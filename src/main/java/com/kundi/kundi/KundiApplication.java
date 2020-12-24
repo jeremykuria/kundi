3a10696f0d0921ede5253384d94a2ed501e55841
Jeremiah kuria <jkaranja@meliora.tech>
2020-12-24 13:53:31 +0300

    project set up

---
+++ src/main/java/com/kundi/kundi/KundiApplication.java
@@ -0,0 +1,13 @@
+package com.kundi.kundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KundiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kundi/kundi/KundiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kundi.kundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KundiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
